eca2d302634f8310686c8068d159952064bd35b6
Hrystyna18 <tymkivhrystyna007@icloud.com>
2020-04-27 21:58:46 +0300

    Add project files.

---
+++ Lab3_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Lab3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StreamWriter file = File.CreateText("file.txt");
+            for (int i = 1; i <= n; ++i)
+            {
+                file.Write("{0} ", 2 * i);
+            }
+            file.Close();
+        }
+    }
+}
